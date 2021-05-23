afc93e201cdb780e85e318d5af11af7816601a2e
Leon Lee <jtllee1@gmail.com>
2021-05-23 18:22:26 +0100

    changed filter bar to use javascript instead of a form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,27 +3,9 @@ class CharactersController < ApplicationController
 
   def index
     @characters = Character.all
-
-    if params[:element].present?
-      @filter = []
-
-      @characters.each do |character|
-        if character.element == params[:element]
-          @filter << character
-        end
-      end
-
-      @characters = @filter
-    end
   end
 
   def show
     @character = Character.find(params[:id])
-  end
-
-  private
-
-  def character_params
-    params.require(:character).permit(:element)
   end
 end
